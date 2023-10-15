0f1dcb147a00878d1c80376e64297b0d28e7a512
WOWmasterJulia <topazzz@ukr.net>
2023-10-16 01:39:48 +0300

    certificates changes

--- js/main.js
+++ js/main.js
@@ -37,8 +37,21 @@ const swiperTwo = new Swiper(".certificates__slider", {
     slidesPerView: 3,
     spaceBetween: 20,
   // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
+    pagination: {
+        el: ".swiper-pagination",
+        },
+    breakpoints: {
+     320: {
+      slidesPerView: 1, 
+        },
+    480: {
+      slidesPerView: 2,
+    //   spaceBetween: 30,
+        }, 
+    640: {
+      slidesPerView: 3,
+    //   spaceBetween: 40,
+    },
   },
 
   // Navigation arrows
@@ -51,4 +64,29 @@ const swiperTwo = new Swiper(".certificates__slider", {
 //   scrollbar: {
 //     el: ".swiper-scrollbar",
 //   },
+
 });
+
+// const swiper = new Swiper(".swiper", {
+//   // Default parameters
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   // Responsive breakpoints
+//   breakpoints: {
+//     // when window width is >= 320px
+//     320: {
+//       slidesPerView: 2,
+//       spaceBetween: 20,
+//     },
+//     // when window width is >= 480px
+//     480: {
+//       slidesPerView: 3,
+//       spaceBetween: 30,
+//     },
+//     // when window width is >= 640px
+//     640: {
+//       slidesPerView: 4,
+//       spaceBetween: 40,
+//     },
+//   },
+// });
