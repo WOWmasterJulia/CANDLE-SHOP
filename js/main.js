642645b036e994469912ff1c743de4a25db9004d
WOWmasterJulia <topazzz@ukr.net>
2023-10-15 17:11:00 +0300

    sertificate

--- js/main.js
+++ js/main.js
@@ -28,3 +28,26 @@ const swiperOne = new Swiper(".feedback__slider", {
 //     el: ".swiper-scrollbar",
 //   },
 });
+
+
+const swiperTwo = new Swiper(".sertificates__slider", {
+  // Optional parameters
+//   direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+  // And if we need scrollbar
+//   scrollbar: {
+//     el: ".swiper-scrollbar",
+//   },
+});
