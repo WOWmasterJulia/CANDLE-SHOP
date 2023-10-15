a23ca502da0ae8804dc2de736bc3ccb04455a65a
WOWmasterJulia <topazzz@ukr.net>
2023-10-15 15:54:59 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,27 @@ const menuMobile = document.querySelector(".header__menu-list");
 menuBtn.addEventListener('click', () => {
     menuMobile.classList.toggle('menu--open');
 });
+
+
+
+const swiperOne = new Swiper(".feedback__slider", {
+  // Optional parameters
+//   direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+  // And if we need scrollbar
+//   scrollbar: {
+//     el: ".swiper-scrollbar",
+//   },
+});
